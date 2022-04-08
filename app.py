af8229ae8942994583277f7c1c7058efb94fe1ea
Jacques Pierre <pierreandassoc.gmail>
2022-04-08 06:15:23 -0600

    feat: display board and a form that allows the user to submit a word

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, session, jsonify
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "this_is_a_secret_key"
 
 boggle_game = Boggle()
+
+@app.route('/')
+def homepage():
+	"""Display board."""
+
+	board = boggle_game.make_board()
+	session["board"] = board
+	return render_template("index.html", board=board)
