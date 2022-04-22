64b5918c030b9bbf0d9175dcfa7867e7fe473031
Jacques Pierre <pierreandassoc.gmail>
2022-04-21 18:22:20 -0600

    feat: when the user submit the form, the word is sent to the server       on the front end, the response from the back end is displaying to notify the user if the word is valid       and exists on the board, if the word is invalid, or if the word does not exist at all

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ app.config["SECRET_KEY"] = "this_is_a_secret_key"
 
 boggle_game = Boggle()
 
-@app.route('/')
+@app.route("/")
 def homepage():
 	"""Display board."""
 
 	board = boggle_game.make_board()
 	session["board"] = board
 	return render_template("index.html", board=board)
+
+@app.route("/check-word")
+def check_word():
+	"""Check if word is in dictionary."""
+
+	word = request.args["word"]
+	board = session["board"]
+	response = boggle_game.check_valid_word(board, word)
+
+	return jsonify({"result": response})
---
+++ static/boggle.js
@@ -0,0 +1,47 @@
+class BoggleGame {
+  /* make a new game at this board id */
+
+  constructor(boardId) {
+    this.words = new Set();
+    this.board = $("#" + boardId);
+    $(".submit", this.board).on("click", this.handleSubmit.bind(this));
+  }
+
+  showWord(word) {
+    /* show word in list of words */
+    $(".words", this.board).append($("<li>", { text: word }));
+  }
+
+  showMessage(msg, cls) {
+    /* show a status message */
+    $(".msg", this.board).text(msg).removeClass().addClass(`msg ${cls}`);
+  }
+
+  async handleSubmit(evt) {
+    /* handle word submission: if unique and valid, score and show */
+    evt.preventDefault();
+    const $word = $(".word", this.board);
+
+    let word = $word.val();
+    if (!word) return;
+
+    if (this.words.has(word)) {
+      this.showMessage(`Already found ${word}`, "err");
+      return;
+    }
+
+    /* chek server for validity */
+    const resp = await axios.get("/check-word", { params: { word: word }});
+    if (resp.data.result === "not-word") {
+      this.showMessage(`${word} is not a valid English word`, "err")
+    } else if (resp.data.result === "not-on-board") {
+      this.showMessage(`${word} is not a valid word on this board`, "err");
+    } else {
+      this.showWord(word);
+      this.words.add(word);
+      this.showMessage(`Added: ${word}`, "ok")
+    }
+
+    $word.val("").focus();
+  }
+}
